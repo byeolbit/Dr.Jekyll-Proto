9549f858294fa02d174dffed89e560196290a9bc
haeungun <haeungun@gmail.com>
2017-11-23 17:15:00 +0900

    Directory error handling

--- src/app/repository/repository.component.ts
+++ src/app/repository/repository.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild, ElementRef, Input} from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { RepositoryService } from './provider/repository.service'
 import { Repository } from './repository'
 import { remote } from 'electron';
@@ -23,8 +23,11 @@ export class RepositoryComponent implements OnInit{
   repoList: Repository[];
   icon_repo = 'assets/images/icon/icon_repo.png';
   repositorySrc;
+  selectedRepo: string;
+  userName: string;
 
   constructor (
+    private route: ActivatedRoute,    
     private router: Router,
     private repositoryService: RepositoryService,
     private jekyllService: JekyllService
@@ -35,26 +38,62 @@ export class RepositoryComponent implements OnInit{
     win.setFullScreenable(false);
     win.setMaximizable(false);
     win.setSize(780,480,true);
-    this.repositoryService.getRepositories()
-                          .then(repository => {
-                            this.repoList = repository;
-                          });
+    let params = this.route.snapshot.params;
+    this.repoList = params.repo_list.split(',');
+    this.userName = params.user;
   }
 
-  loadRepository() {
-    let openProject = this.selectDirectory(this.jekyllService);
+  loadRepository(): void {
+    let openProject = this.selectDirectory(
+      this.jekyllService, this.repositoryService,
+      this.selectedRepo, this.userName);
     
-    openProject.then(this.runJekyll)
-               .then(result => this.navigateToEditor(result));
+    openProject
+    .then(this.cloneRepo)
+    .then(this.runJekyll)
+    .then(result => this.navigateToEditor(result))
+    .catch((error: Error) => {
+      // Not implemented yet
+    });
   }
 
-  private selectDirectory(service) {
-    return new Promise((resolve, rejected)=>{
-      let dialog = remote.dialog;
-      resolve({
-        path: dialog.showOpenDialog({properties: ['openDirectory']}).toString(),
-        service: service
-      })
+  selectRepo(event: Event) {
+    const target = event.target.innerText.trim();
+    if (target) {
+      this.selectedRepo = target;
+    }
+  }
+
+  private selectDirectory(service:JekyllService,
+                          repoService:RepositoryService,
+                          repo:string, user:string): Promise<any> {
+    return new Promise((resolve, reject) => {
+      const dialog = remote.dialog;
+      const directory = dialog.showOpenDialog({properties: ['openDirectory']});
+      
+      if (directory) {
+        resolve({
+          path: directory.toString(),
+          repoService: repoService,
+          service: service,
+          repo: repo,
+          user: user
+        })
+      } else {
+        reject("No directory selected");
+      }
+    });
+  }
+
+  private cloneRepo(recived) {
+    return new Promise ((resolve, rejected) => {
+      recived.repoService
+             .cloneRepository(recived.user, recived.repo, recived.path)
+             .then(res => {
+                if(res.success){
+                 resolve(recived);
+                }
+             });
     });
   }
   
@@ -70,7 +109,7 @@ export class RepositoryComponent implements OnInit{
     });
   }
 
-  private navigateToEditor(result) {
+  private navigateToEditor(result: any): void {
     if (result.success) {
       this.router.navigate(['/editor',{'src' : result.path }]);
       this.jekyllService.passJekyllProcess(result.child);
