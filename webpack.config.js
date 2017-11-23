94aa43b52bb915b1c91f3f0cc57005d9ba3ff02c
haeungun <haeungun@gmail.com>
2017-11-23 17:38:27 +0900

    Dr.Hyde copied into dist folder by webpack

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,14 @@ function getPlugins() {
   // Always expose NODE_ENV to webpack, you can now use `process.env.NODE_ENV`
   // inside your code for any environment checks; UglifyJS will automatically
   // drop any unreachable code.
+
+  plugins.push(new CopyWebpackPlugin ([
+    { 
+      from: path.join(__dirname, "Dr.Hyde"), 
+      to: "Dr.Hyde/" 
+    }
+  ]));
+
   plugins.push(new DefinePlugin({
     "process.env.NODE_ENV": "\"production\""
   }));
